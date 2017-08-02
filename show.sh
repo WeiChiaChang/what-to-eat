5016b15b5803205b47fca2cbb5fd96a9642eac16
WesleyChang <ioh04019@gmail.com>
2017-08-02 13:50:32 +0800

    🐛 fix bugs about node.js path

--- show.sh
+++ show.sh
@@ -3,7 +3,7 @@
 # 找到相對應的目錄
 cd $HOME/Desktop/what-to-eat
 # 執行 main.js 取得隨機店家，並 assign 給 lunch
-lunch=`node main.js`
+lunch=`/usr/local/bin/node main.js`
 # 跑 Mac 內建的 script，製造推播推知
 osascript -e "display notification \"$lunch\" with title \"今天午餐吃什麼 ?\""
 # 我叫 Mac 唸給妳聽！
